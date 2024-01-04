3c291a5b741fc53edc3afc22841b5b2dffa87477
false-fox <154926234+false-fox@users.noreply.github.com>
2024-01-04 02:07:26 -0800

    add test

---
+++ test/test.js
@@ -0,0 +1,10 @@
+const falsefox = require("../index") 
+
+console.log(falsefox())
+// expected:
+// {
+//    name: 'falsefox',
+//    github: 'false-fox',
+//    website: 'falsefox.dev',
+//    npm: 'falsefox'
+// }
