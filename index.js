c445c39003eadae74992362df38da280129ea70a
false-fox <154926234+false-fox@users.noreply.github.com>
2024-01-04 01:56:14 -0800

    Init

---
+++ index.js
@@ -0,0 +1,10 @@
+function falsefox() {
+    return {
+        "name": "falsefox",
+        "github": "false-fox",
+        "website": "falsefox.dev",
+        "npm": "falsefox"
+    }
+}
+
+module.exports = falsefox
